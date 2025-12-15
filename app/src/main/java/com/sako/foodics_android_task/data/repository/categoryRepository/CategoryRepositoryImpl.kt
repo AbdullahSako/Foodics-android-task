e550475c9d05c007a58cfd0a3852c66cae8ba680
AbdullahSako <82209465+AbdullahSako@users.noreply.github.com>
2025-12-16 01:55:05 +0300

    fix an issue where the app would crash when an unhandled exception is thrown on api call

--- app/src/main/java/com/sako/foodics_android_task/data/repository/categoryRepository/CategoryRepositoryImpl.kt
+++ app/src/main/java/com/sako/foodics_android_task/data/repository/categoryRepository/CategoryRepositoryImpl.kt
@@ -5,20 +5,15 @@ import com.sako.foodics_android_task.data.Constants
 import com.sako.foodics_android_task.data.model.external.Category
 import com.sako.foodics_android_task.data.model.network.NetworkCategory
 import com.sako.foodics_android_task.data.model.network.toExternal
-import com.sako.foodics_android_task.utils.resultWrapper.Result
-import com.sako.foodics_android_task.utils.ext.logd
 import com.sako.foodics_android_task.utils.resultWrapper.NetworkError
+import com.sako.foodics_android_task.utils.resultWrapper.Result
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 import io.ktor.util.network.UnresolvedAddressException
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
 import kotlinx.serialization.SerializationException
 import org.koin.core.annotation.InjectedParam
 
@@ -39,6 +34,10 @@ class CategoryRepositoryImpl(@InjectedParam val httpClient: HttpClient) : Catego
         } catch (e: SerializationException) {
             emit(Result.Error(errorType = NetworkError.SERIALIZATION))
             return@flow
+        } catch (e: Exception) {
+            e.printStackTrace()
+            emit(Result.Error(errorType = NetworkError.UNKNOWN))
+            return@flow
         }
 
         val result = when (response.status.value) {
@@ -46,6 +45,7 @@ class CategoryRepositoryImpl(@InjectedParam val httpClient: HttpClient) : Catego
                 val categoryList = response.body<List<NetworkCategory>>()
                 Result.Success(data = categoryList.map { it.toExternal() })
             }
+
             401 -> Result.Error(errorType = NetworkError.UNAUTHORIZED)
             409 -> Result.Error(errorType = NetworkError.CONFLICT)
             408 -> Result.Error(errorType = NetworkError.REQUEST_TIMEOUT)
