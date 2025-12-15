0ce09b22f64089960a4216c67df6c0e89f4811bc
AbdullahSako <82209465+AbdullahSako@users.noreply.github.com>
2025-12-15 22:38:48 +0300

    fix api result wrapper where error message was not accessible

--- app/src/main/java/com/sako/foodics_android_task/utils/Result.kt
+++ app/src/main/java/com/sako/foodics_android_task/utils/Result.kt
@@ -1,6 +1,6 @@
 package com.sako.foodics_android_task.utils
 
-sealed class Result<T>(val data:T? = null, message:String? = null){
+sealed class Result<T>(val data:T? = null,val message:String? = null){
     class Loading<T>: Result<T>()
     class Success<T>(data:T):Result<T>(data = data)
     class Error<T>(message:String): Result<T>(message = message)
